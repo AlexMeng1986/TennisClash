0c11469937399d2cd1d5c3cbfd8381d0728432e6
fanxin.meng <AlexMeng1986@gmail.com>
2017-05-25 14:42:32 +0800

    【TennisClash】提交测试代码和资源

---
+++ Assets/Script/BallMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float height;
+    private float latest = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        height = 5;
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float now = Time.realtimeSinceStartup;
+        if (now - latest < 0.1f)
+        {
+            return;
+        }
+        latest = now;
+
+        if (collision.gameObject.tag == "Racket" || collision.gameObject.tag == "Wall")
+        {
+            int directionMod = collision.gameObject.tag == "Wall" ? 1 : -1;
+            float x = rb.transform.position.x > -1 && rb.transform.position.x < 1 ? 0 : rb.transform.position.x;
+            int pos = rb.transform.position.x > -1 && rb.transform.position.x < 1 ? 0 : rb.transform.position.x > 0 ? 1 : -1;
+            rb.transform.position = new Vector3(x, height, collision.gameObject.transform.position.z - directionMod * 0.8f);
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            int min = -1 - directionMod * (int)pos;
+            int max = 2 - directionMod * (int)pos;
+            int xMod = Random.Range(min, max);
+
+            rb.AddForce(new Vector3(directionMod * xMod * 100, 140, -directionMod * 4 * 200));
+        }
+    }
+}
---
+++ Assets/Script/RacketMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour {
+
+	private Transform trans = null;
+
+    private void OnEnable()
+    {
+        //EasyTouch.On_SwipeStart += On_SwapStart;
+        //EasyTouch.On_Swipe += On_Swap;
+        //EasyTouch.On_SwipeEnd += On_SwapEnd;
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeEvent();
+    }
+
+    private void OnDestroy()
+    {
+        UnsubscribeEvent();
+    }
+
+    void UnsubscribeEvent()
+    {
+        //EasyTouch.On_SwipeStart -= On_SwapStart;
+        //EasyTouch.On_Swipe -= On_Swap;
+        //EasyTouch.On_SwipeEnd -= On_SwapEnd;
+    }
+
+	void Start() {
+		trans = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			trans.position += Vector3.left * 3.2f;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			trans.position += Vector3.right * 3.2f;
+		} 
+		 
+			
+	}
+
+    //private void On_SwapStart(Gesture gesture)
+    //{
+
+    //}
+
+    //private void On_Swap(Gesture gesture)
+    //{
+
+    //}
+
+    //private void On_SwapEnd(Gesture gesture)
+    //{
+    //    Debug.Log(gesture.swipeVector.normalized);
+    //}
+}
