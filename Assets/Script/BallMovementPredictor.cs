dc16459b39b4075c3f701be6d5c9bfa8a62b5f71
fanxin.meng <AlexMeng1986@gmail.com>
2017-05-26 18:38:26 +0800

    【TennisClash】添加球运动轨迹与场景的碰撞检测

--- Assets/Script/BallMovementPredictor.cs
+++ Assets/Script/BallMovementPredictor.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using AAX;
 
+
 public class BallMovementPredictor : MonoBehaviour {
 	private LineRenderer lr;
 	private Rigidbody rb;
 
 	public float timeBetweenStep = 1;
 	public int stepCount = 50;
+	public Vector3 addedSpeed;
+	public Vector3 addedForce;
+	public bool checkForCollision = true;
+
+	public LayerMask raycastMask = -1;
+
+	[HideInInspector]
+	public RaycastHit hitInfo;
+
+	[HideInInspector]
+	public List<Vector3> predictionPoints = new List<Vector3> ();
 
 
 	// Use this for initialization
@@ -22,7 +34,7 @@ public class BallMovementPredictor : MonoBehaviour {
 	void Update () {
 		DrawMovementLine();
 	}
-
+	/*
 	private void DrawMovementLine()
 	{
 		var res = rb.CalculateMovement(stepCount, timeBetweenStep);
@@ -32,6 +44,66 @@ public class BallMovementPredictor : MonoBehaviour {
 		for (int i = 0; i < res.Length; ++i)
 		{
 			lr.SetPosition(i+1, res[i]);
+		}
+
+	}
+    */
+	private void DrawMovementLine()
+	{
+		PerformPrediction ();
+
+
+		Vector3[] res = predictionPoints.ToArray ();
+		lr.positionCount = res.Length;
+		for (int i = 0; i < res.Length; ++i)
+		{
+			lr.SetPosition(i, res[i]);
+		}
+
+	}
+
+	private void PerformPrediction() {
+		float dt = Time.fixedDeltaTime;
+		Vector3 v = (rb.isKinematic == false ? rb.velocity : Vector3.zero);
+		Vector3 acc = (rb.useGravity && rb.isKinematic == false ? Physics.gravity : Vector3.zero);
+
+		Vector3 addedV = (addedForce / rb.mass) * dt;
+		v = v + addedSpeed + addedV;
+
+
+		bool done = false;
+		int iter = 0;
+		predictionPoints.Clear ();
+
+		Vector3 pos = rb.transform.position;
+		Vector3 toPos = pos;
+		Vector3 dir = Vector3.zero;
+
+		while (!done && iter < stepCount) {
+			var aDt = acc * dt;
+			var dragDt = 1 - rb.drag * dt;
+			dragDt = dragDt < 0 ? 0 : dragDt;
+
+			for (int i = 0; i < timeBetweenStep; ++i) {
+				v = (v + aDt) * dragDt;
+				toPos = pos + v * dt + .5f * acc * dt * dt;
+			}
+
+			dir = toPos - pos;
+			predictionPoints.Add (pos);
+
+			float dist = Vector3.Distance(pos, toPos);
+			if (checkForCollision) {
+				Ray ray = new Ray(pos, dir);
+				if (Physics.Raycast (ray, out hitInfo, dist, raycastMask)) {
+					predictionPoints.Add (hitInfo.point);
+					done = true;
+				}			
+			}
+
+			pos = toPos;
+			++iter;
+
 		}
 
 	}
